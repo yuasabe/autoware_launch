0a2dafc5ba43e763b425a6d885f4b6edf22133e6
kminoda <koji.m.minoda@gmail.com>
2022-08-17 17:49:55 +0900

    now workflow creates sync-param-files.yaml
    Signed-off-by: kminoda <koji.m.minoda@gmail.com>

--- .github/workflows/generate_sync_param_files.py
+++ .github/workflows/generate_sync_param_files.py
@@ -1,13 +1,19 @@
 import glob
 import yaml
+import argparse
 
 def main():
-    tier4_launch_path = "/home/minoda/git/autoware/src/universe/autoware.universe/launch/"
+    tier4_launch_path = "/tmp/autoware.universe/launch/"
     files = glob.iglob(tier4_launch_path + '**/*.param.yaml', recursive=True)
 
     src2dst = []
-    for src in files:
-        dst_list = ['autoware_launch', 'config'] + src.split('/')[9:]
+    for file in files:
+        src_list = file.split('/')[3:]
+        dst_list = ['autoware_launch', 'config'] + file.split('/')[4:5] + file.split('/')[6:]
+        src = ''
+        for el in src_list:
+            src += el + '/'
+        src = src[:-1]
         dst = ''
         for el in dst_list:
             dst += el + '/'
@@ -16,13 +22,13 @@ def main():
             "source": src,
             "dest": dst
         })
-    
+
     sync_param_file = [{
         "repository": "autowarefoundation/autoware.universe",
         "files": src2dst
     }]
 
-    with open("sync-param-files.yaml", "w") as f:
+    with open("hogehoge-sync-param-files.yaml", "w") as f:
         yaml.dump(sync_param_file, f, sort_keys=False)
 
 if __name__ == "__main__":
---
+++ .github/workflows/generate_sync_param_files.sh
@@ -0,0 +1,2 @@
+git clone https://github.com/autowarefoundation/autoware.universe.git /tmp/autoware.universe
+python3 generate_sync_param_files.py
